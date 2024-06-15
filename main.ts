b2e9c1a2d52db0372f15a0bdcd2311cb9520fa66
Doug Richardson <git@rekt.email>
2024-06-14 22:23:47 -0700

    remove noisy log

--- main.ts
+++ main.ts
@@ -6,7 +6,6 @@ export default class ToggleReadableLineLengthPlugin extends Plugin {
 			id: 'toggle-readable-line-length-command',
 			name: 'Toggle Readable line length',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log("Toggle Readable line length");
 				// @ts-ignore
 				const optionEnabled = this.app.vault.getConfig("readableLineLength");
 				// @ts-ignore
