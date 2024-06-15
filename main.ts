c306e8e37cc31911dbf8875c7b36a233dd85ccfe
Doug Richardson <git@rekt.email>
2024-06-14 22:39:41 -0700

    Set hotkey; fix toggle in reading mode; format code. (#7)

--- main.ts
+++ main.ts
@@ -1,16 +1,20 @@
-import { Editor, MarkdownView, Plugin } from 'obsidian';
+import { Editor, MarkdownView, Plugin } from "obsidian";
 
 export default class ToggleReadableLineLengthPlugin extends Plugin {
 	async onload() {
 		this.addCommand({
-			id: 'toggle-readable-line-length-command',
-			name: 'Toggle Readable line length',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
+			id: "toggle-readable-line-length-command",
+			name: "Toggle Readable line length",
+			// Obsidian developer docs suggest not setting hotkeys, but this is the entire point of this plugin, so ignoring that advice.
+			// https://docs.obsidian.md/Plugins/User+interface/Commands#Hot%20keys
+			hotkeys: [{ modifiers: ["Mod", "Shift"], key: "e" }],
+			callback: () => {
 				// @ts-ignore
-				const optionEnabled = this.app.vault.getConfig("readableLineLength");
+				const optionEnabled =
+					this.app.vault.getConfig("readableLineLength");
 				// @ts-ignore
 				this.app.vault.setConfig("readableLineLength", !optionEnabled);
-			}
+			},
 		});
 	}
 }
