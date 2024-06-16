74822036f76750a745f26695ed63c3ea914c6a46
Doug Richardson <git@rekt.email>
2024-06-16 10:24:01 -0700

    forgot one thing

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import { Editor, MarkdownView, Plugin } from "obsidian";
 export default class ToggleReadableLineLengthPlugin extends Plugin {
 	async onload() {
 		this.addCommand({
-			id: "toggle-readable-line-length-setting",
+			id: "flip-readable-line-length-setting",
 			name: "Toggle Readable line length",
 			// Obsidian developer docs suggest not setting hotkeys, but this is the entire point of this plugin, so ignoring that advice.
 			// https://docs.obsidian.md/Plugins/User+interface/Commands#Hot%20keys
