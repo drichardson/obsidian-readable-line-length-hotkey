06768f9b42faf798c1f8b6dde134da6c74892d89
Doug Richardson <git@rekt.email>
2024-06-18 09:49:12 -0700

    Change command name to be different than plugin name.
    Addressing feedback from https://github.com/obsidianmd/obsidian-releases/pull/3701#issuecomment-2175375855

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ export default class ToggleReadableLineLengthPlugin extends Plugin {
 	async onload() {
 		this.addCommand({
 			id: "flip-readable-line-length-setting",
-			name: "Toggle Readable line length",
+			name: "Toggle",
 			// Obsidian developer docs suggest not setting hotkeys, but this is the entire point of this plugin, so ignoring that advice.
 			// https://docs.obsidian.md/Plugins/User+interface/Commands#Hot%20keys
 			hotkeys: [{ modifiers: ["Mod", "Shift"], key: "e" }],
